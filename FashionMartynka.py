5cb1a23464f2239b46637077fbcad875c0f36c74
radosan <radoslaw.sanetra@gmail.com>
2024-04-12 21:57:37 +0200

    version 3

--- FashionMartynka.py
+++ FashionMartynka.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -31,6 +33,19 @@ def fit_predict_score(model, x_train, y_train, x_test, y_test, verbose=False, sh
     return {'train_score': acc_train, 'test_score': acc_test, 'train_time': run_time}
 
 
+def plot_graph(data):
+    df = pd.DataFrame(data)
+    x = np.arange(df.shape[0])
+    fig, ax = plt.subplots()
+    ax.bar(x - 0.2, height=df.loc['test_score'], width=0.4, label='accuracy')
+    ax.bar(x + 0.2, height=df.loc['train_time'] / df.loc['train_time'].max(), width=0.4, label='train time')
+    fig.suptitle('relative train time and accuracy of different models')
+    ax.set_ylabel('accuracy score')
+    ax.set_xticks(x, [name.split('(')[0] for name in df.columns])
+    ax.legend()
+    plt.show()
+
+
 def main():
     # prepare datasets
     train_set = pd.read_csv('data/fashion-mnist_train.csv')
@@ -40,16 +55,36 @@ def main():
     x_test = scale(test_set.drop('label', axis=1))
     y_test = test_set['label']
 
-    models = [MLPClassifier(hidden_layer_sizes=(60, 80, 60), solver='adam', alpha=0.01),
+    # parameter space for grid search
+    knn_params = {'n_neighbors': [3, 5, 51], 'algorithm': ['auto', 'brute']}
+    tree_params = {'criterion': ['gini', 'entropy'], 'max_depth': [50, 180, None]}
+
+    # include grid search in model comparison
+    use_grid = False
+
+    # set grid search for models
+    knn_grid = GridSearchCV(estimator=KNeighborsClassifier(), param_grid=knn_params, scoring='accuracy', n_jobs=-1)
+    tree_grid = GridSearchCV(estimator=DecisionTreeClassifier(), param_grid=tree_params, scoring='accuracy', n_jobs=-1)
+
+    # models to compare
+    models = [MLPClassifier(hidden_layer_sizes=(60, 80, 60), solver='adam', alpha=0.01, max_iter=5),
               KNeighborsClassifier(n_neighbors=3),
               DecisionTreeClassifier(criterion='gini', max_depth=80)]
+    if use_grid:
+        models.extend([knn_grid, tree_grid])
+
+    # train models, check accuracy and training time
     scores = {}
     for model in models:
         scores[str(model)] = fit_predict_score(model, x_train, y_train, x_test, y_test,
                                                verbose=True, showtime=True)
 
+    # print and save results to file
     print(scores)
+    if use_grid:
+        print('Best grid search models:', knn_grid.best_estimator_, tree_grid.best_estimator_, sep='\n')
     save_results(scores, append=True)
+    plot_graph(scores)
 
 
 if __name__ == '__main__':
